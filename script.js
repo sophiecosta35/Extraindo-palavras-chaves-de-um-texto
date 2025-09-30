b5ec26f74c5cdc33b4fb3dcdf4b12921374f71b6
sophie costa <sophie.domingues@escola.pr.gov.br>
2025-09-30 14:19:23 -0300

    .

--- script.js
+++ script.js
@@ -10,7 +10,24 @@ function mostraPalavrasChave (){
     campoResultado.textContent = palavrasChave.join(", ");
 }
 
+
 function processaTexto(texto){
-    let palavras = texto.split(" ");
-    return palavras; 
+    let palavras = texto.split(/\P{L}+/u);
+   const frequencias = contafrequencias(palavras);
+   let ordenadas = object.keys(frequencias)
+   return palavras; 
+}
+
+function contafrequencias(palavras){
+let frequencias = {};
+   for(let i of palavras){
+    frequencias[i] = 0;
+    for (let j of palavras){
+        if (i == j){
+            frequencias[i]++;
+        }
+    }
+   }
+   
+   return frequencias;
 }
