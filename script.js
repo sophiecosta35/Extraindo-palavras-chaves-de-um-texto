245ca60e0c995a408d661e70b8bbebd14820eebc
sophie costa <sophie.domingues@escola.pr.gov.br>
2025-10-02 10:48:40 -0300

    .

--- script.js
+++ script.js
@@ -14,8 +14,14 @@ function mostraPalavrasChave (){
 function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
    const frequencias = contafrequencias(palavras);
-   let ordenadas = object.keys(frequencias)
-   return palavras; 
+   let ordenadas = object.keys(frequencias).sort(ordenapalavra);
+
+function ordenapalavra(p1,p2){
+    return frequencias[p2] - frequencias[p1];
+}
+
+   console.log(ordenadas);
+   return ordenadas.slice(0,10); 
 }
 
 function contafrequencias(palavras){
