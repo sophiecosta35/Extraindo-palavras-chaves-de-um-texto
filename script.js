b723d38e745a81c8a66a70670f1faa23550653f3
sophie costa <sophie.domingues@escola.pr.gov.br>
2025-10-02 11:14:19 -0300

    .

--- script.js
+++ script.js
@@ -6,20 +6,22 @@ function mostraPalavrasChave (){
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavra-chave');
     const palavrasChave = processaTexto(texto);
-    
     campoResultado.textContent = palavrasChave.join(", ");
 }
 
-
 function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
+for(let i in palavras){
+    palavras[i] = palavras[i].tolowercase();
+}
+    
+palavras = tirapalavrasruins(palavras);
+
    const frequencias = contafrequencias(palavras);
    let ordenadas = object.keys(frequencias).sort(ordenapalavra);
-
 function ordenapalavra(p1,p2){
     return frequencias[p2] - frequencias[p1];
 }
-
    console.log(ordenadas);
    return ordenadas.slice(0,10); 
 }
@@ -34,6 +36,16 @@ let frequencias = {};
         }
     }
    }
-   
-   return frequencias;
+    return frequencias;
+}
+
+function tirapalavrasruins(palavras) {
+    const PALAVRAS_RUINS = ["para", "uma", "nós"];
+const palavrasboas = [];
+for (let palavra of palavras) {
+    if (! PALAVRAS_RUINS.has(palavra)&& palavra.length > 2) {
+        palavrasBoas.push(palavra);
+    }
+}
+return palavrasBoas;
 }
